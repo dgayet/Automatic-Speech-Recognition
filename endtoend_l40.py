bab96c73404f3a7c2d1eb7dc76c96a07a3c077d8
dgayet <dgayet@fi.uba.ar>
2023-07-31 06:46:12 -0300

    draft of main

--- endtoend_l40.py
+++ endtoend_l40.py
@@ -4,17 +4,20 @@ import torchaudio
 import torchaudio.transforms as T
 import torchaudio.functional as F
 import torch.nn as nn
+import torch.utils.data as data
 import torch.optim as optim
 import numpy as np
 from matplotlib import pyplot as plt
 from preprocessing import *
 from Classes import *
 from Model import SpeechRecognitionModel
-from Decoder import GreedyDecoder
+from engine import train
 
 # GPU computing
+use_cuda = False
 if torch.cuda.is_available():
     device = torch.device("cuda")
+    use_cuda = True
 elif torch.backends.mps.is_available():
     device = torch.device("mps")
 else:
@@ -38,20 +41,29 @@ hparams={
     "batch_size": batch_size,
     "epochs": epochs
 }
-#%%
+
+# Loading Dataset
 train_dataset = Latino40Dataset('./dataset/train.json', './dataset')
 test_dataset = Latino40Dataset('./dataset/valid.json', './dataset')
 
-train_audio_transforms = nn.Sequential(
-    T.MelSpectrogram(sample_rate=16e3, n_mels=128),
-    T.FrequencyMasking(freq_mask_param=15),
-    T.TimeMasking(time_mask_param=35)
-)
-valid_audio_transforms = T.MelSpectrogram()
+kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
+train_loader = data.DataLoader(dataset=train_dataset,
+                               batch_size=hparams['batch_size'],
+                               shuffle=True,
+                               collate_fn= lambda x: data_processing(x, 'train'),
+                               **kwargs)
+test_loader = data.DataLoader(dataset=test_dataset,
+                              batch_size=hparams['batch_size'],
+                              shuffle=False,
+                              collate_fn=lambda x: data_processing(x, 'valid'),
+                              **kwargs)
 
 text_transform = TextTransforms()
 
-model = SpeechRecognitionModel()
+model = SpeechRecognitionModel(
+    hparams['n_cnn_layers'], hparams['n_rnn_layers'], hparams['rnn_dim'],
+    hparams['n_class'], hparams['n_feats']).to(device)
+
 optimizer = optim.AdamW(model.parameters(), hparams['learning_rate'])
 criterion = nn.CTCLoss(blank=28).to(device)
 scheduler = optim.lr_scheduler.OneCycleLR(optimizer, max_lr=hparams['learning_rate'],
@@ -60,7 +72,6 @@ scheduler = optim.lr_scheduler.OneCycleLR(optimizer, max_lr=hparams['learning_ra
                                           anneal_strategy="linear")
 
 # iter_meter = IterMeter()
-for epoch in range(1, epochs +1):
-    train(model, device, train_loader, criterion, optimizer, scheduler, epoch)
-    if (epoch % 10 == 0):
-        test(model, device, test_loader, criterion, epoch)
+train(model, device, train_loader, criterion, optimizer, scheduler, test_loader, hparams['epochs'])
+
+# %%
