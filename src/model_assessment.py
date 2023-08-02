d1733fe63c0dcb02e5247738a8e15db90b62a425
dgayet <dgayet@fi.uba.ar>
2023-08-02 04:25:55 -0300

    updated model_assessment.py

--- src/model_assessment.py
+++ src/model_assessment.py
@@ -37,6 +37,8 @@ with open('training_logs/losses_log_BATCHSIZE_8_CNNL_5_RNNL_1.json') as jsonfile
     losses851 = json.load(jsonfile)
 with open('training_logs/losses_log_BATCHSIZE_8_CNNL_8_RNNL_1.json') as jsonfile:
     losses881 = json.load(jsonfile)
+with open('training_logs/losses_log_BATCHSIZE_8_CNNL_10_RNNL_3.json') as jsonfile:
+    losses8103 = json.load(jsonfile)
 
 #%%
 [fig,ax] = plt.subplots(figsize=(10,10))
@@ -46,6 +48,7 @@ ax.plot(np.arange(1,101,10), losses825[0], linewidth=2, label='CNN Layers=2, NRN
 ax.plot(np.arange(1,101,10), losses831[0], linewidth=2, label='CNN Layers=3, NRNN Layers=1, Batch Size=8')
 ax.plot(np.arange(1,101,10), losses851[0], linewidth=2, label='CNN Layers=5, NRNN Layers=1, Batch Size=8')
 ax.plot(np.arange(1,101,10), losses881[0], linewidth=2, label='CNN Layers=8, NRNN Layers=1, Batch Size=8')
+ax.plot(np.arange(1,101,10), losses8103[0], linewidth=2, label='CNN Layers=10, NRNN Layers=3, Batch Size=8')
 
 
 ax.grid()
@@ -59,6 +62,8 @@ ax.plot(losses825[1], linewidth=2, label='CNN Layers=2, NRNN Layers=5, Batch Siz
 ax.plot(losses831[1], linewidth=2, label='CNN Layers=3, NRNN Layers=1, Batch Size=8')
 ax.plot(losses851[1], linewidth=2, label='CNN Layers=5, NRNN Layers=1, Batch Size=8')
 ax.plot(losses881[1], linewidth=2, label='CNN Layers=8, NRNN Layers=1, Batch Size=8')
+ax.plot(losses8103[1], linewidth=2, label='CNN Layers=10, NRNN Layers=3, Batch Size=8')
+
 
 
 ax.set_title("Train loss")
@@ -71,15 +76,16 @@ wer_test825 = np.mean(losses825[2], 1)
 wer_test831 = np.mean(losses831[2], 1)
 wer_test851 = np.mean(losses851[2], 1)
 wer_test881 = np.mean(losses881[2], 1)
-
+wer_test8103 = np.mean(losses8103[2], 1)
 
 [fig,ax] = plt.subplots(figsize=(10,10))
 ax.plot(np.arange(1,101,10), wer_test821, linewidth=2, label='CNN Layers=2, NRNN Layers=1, Batch Size=8')
 ax.plot(np.arange(1,101,10), wer_test824, linewidth=2, label='CNN Layers=2, NRNN Layers=4, Batch Size=8')
-ax.plot(np.arange(1,101,10), wer_test825, linewidth=2, label='CNN Layers=2, NRNN Layers=4, Batch Size=8')
+ax.plot(np.arange(1,101,10), wer_test825, linewidth=2, label='CNN Layers=2, NRNN Layers=5, Batch Size=8')
 ax.plot(np.arange(1,101,10), wer_test831, linewidth=2, label='CNN Layers=3, NRNN Layers=1, Batch Size=8')
 ax.plot(np.arange(1,101,10), wer_test851, linewidth=2, label='CNN Layers=5, NRNN Layers=1, Batch Size=8')
 ax.plot(np.arange(1,101,10), wer_test881, linewidth=2, label='CNN Layers=8, NRNN Layers=1, Batch Size=8')
+ax.plot(np.arange(1,101,10), wer_test8103, linewidth=2, label='CNN Layers=10, NRNN Layers=3, Batch Size=8')
 
 
 ax.set_title("Word Error Rate")
@@ -89,9 +95,50 @@ ax.legend(fontsize=16)
 # %% time
 # (BS, CNN, RNN)
 times = {
-    'CNN n=2, RNN n=1, BS=8' : 4100.24,
-    'CNN n=2, RNN n=4, BS=8' : 7337.12,
-    'CNN n=3, RNN n=1, BS=8' : 5212.45,
-    'CNN n=5, RNN n=1, BS=8' : 6913.45,
-    'CNN n=8, RNN n=1, BS=8' : 9630.55
+    'CNN n=2, RNN n=1, BS=8' : 4100.24/60,
+    'CNN n=2, RNN n=4, BS=8' : 7337.12/60,
+    'CNN n=3, RNN n=1, BS=8' : 5212.45/60,
+    'CNN n=5, RNN n=1, BS=8' : 6913.45/60,
+    'CNN n=8, RNN n=1, BS=8' : 9630.55/60
 }
+times = dict(sorted(times.items(), key=lambda item: item[1]))
+
+parameters = {
+    'CNN n=2, RNN n=1, BS=8' : 4779485,
+    'CNN n=2, RNN n=4, BS=8' : 18959837,
+    'CNN n=2, RNN n=5, BS=8' : 23686621,
+    'CNN n=3, RNN n=1, BS=8' : 4798237,
+    'CNN n=3, RNN n=4, BS=8' : 18978589,
+    'CNN n=5, RNN n=1, BS=8' : 4835741,
+    'CNN n=8, RNN n=1, BS=8' : 4891997,
+    'CNN n=10, RNN n=3, BS=8' : 14383069
+}
+parameters = dict(sorted(list(parameters.items()), key=lambda item: item[1]))
+
+[fig,ax] = plt.subplots(figsize=(10,10))
+ax.bar(range(len(times)), times.values(), tick_label=times.keys())
+ax.set_xticklabels(times.keys(), rotation = 20)
+ax.set_title("Tiempo necesario para entrenar (en minutos)", fontsize=15)
+
+[fig,ax] = plt.subplots(figsize=(10,10))
+ax.bar(range(len(parameters)), parameters.values(), tick_label=parameters.keys())
+ax.set_xticklabels(parameters.keys(), rotation = 45)
+ax.set_title("Parámetros totales de la red (en minutos)", fontsize=15)
+
+
+wer_avg ={
+    'CNN n=2, RNN n=1, BS=8' : wer_test821[-1],
+    'CNN n=2, RNN n=4, BS=8' : wer_test824[-1],
+    'CNN n=2, RNN n=5, BS=8' : wer_test825[-1],
+    'CNN n=3, RNN n=1, BS=8' : wer_test831[-1],
+    'CNN n=5, RNN n=1, BS=8' : wer_test851[-1],
+    'CNN n=8, RNN n=1, BS=8' : wer_test881[-1],
+    'CNN n=10, RNN n=3, BS=8' : wer_test8103[-1]}
+wer_avg = dict(sorted(list(wer_avg.items()), key=lambda item: item[1]))
+
+
+[fig,ax2] = plt.subplots(figsize=(10,10))
+ax2.bar(range(len(wer_avg)), wer_avg.values(), tick_label=wer_avg.keys())
+ax2.set_xticklabels(wer_avg.keys(), rotation = 45)
+ax2.set_title("CER Promedio", fontsize=15)
+# %%
